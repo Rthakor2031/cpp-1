0e8615b9f43c7775f00181d69d5343437a3bbc11
Akshar Patel <143181114+Aksharpatel06@users.noreply.github.com>
2023-11-07 18:46:49 +0530

    Update employee.cpp

--- day 2/employee.cpp
+++ day 2/employee.cpp
@@ -1,16 +1,11 @@
 #include<iostream>
+using namespace std;
 class Employee
 {
 	public:
-	int emp_id;
-	string emp_name;
-	int emp_age;
-	string emp_role;
-	int emp_salary;
-    string emp_city;
-	int emp_experience;
-	string emp_company_name;
-}
+	int emp_id,emp_age,emp_salary,emp_experience;
+	string emp_name,emp_role,emp_city,emp_company_name;
+};
 main()
 {
 	Employee a1;
@@ -103,10 +98,10 @@ main()
 	cout << "company name:";
 	cin >> a5.emp_company_name;
 	
-	cout << "id" << "name" << "age" << "role" << "salary" << "city" << "experience" << "company name" << endl;
-	cout << a1.emp_id << a1.emp_name << a1.emp_age << a1.emp_role << a1.emp_salary << a1.emp_city << a1.emp_experience << a1.emp_company_name << endl;
-	cout << a2.emp_id << a2.emp_name << a2.emp_age << a2.emp_role << a2.emp_salary << a2.emp_city << a2.emp_experience << a2.emp_company_name << endl;
-	cout << a3.emp_id << a3.emp_name << a3.emp_age << a3.emp_role << a3.emp_salary << a3.emp_city << a3.emp_experience << a3.emp_company_name << endl;
-	cout << a4.emp_id << a4.emp_name << a4.emp_age << a4.emp_role << a4.emp_salary << a4.emp_city << a4.emp_experience << a4.emp_company_name << endl;
-	cout << a5.emp_id << a5.emp_name << a5.emp_age << a5.emp_role << a5.emp_salary << a5.emp_city << a5.emp_experience << a5.emp_company_name << endl;
+	cout << "id\t" << "name\t" << "age\t" << "role\t" << "salary\t" << "city\t" << "experience\t" << "company name\t" << endl;
+	cout << a1.emp_id << "\t" <<a1.emp_name <<"\t" << a1.emp_age <<"\t" << a1.emp_role <<"\t" << a1.emp_salary <<"\t" << a1.emp_city <<"\t" << a1.emp_experience <<"\t" << a1.emp_company_name << endl;
+	cout << a2.emp_id << "\t" <<a2.emp_name <<"\t" << a2.emp_age <<"\t" << a2.emp_role <<"\t" << a2.emp_salary <<"\t" << a2.emp_city <<"\t" << a2.emp_experience <<"\t" << a2.emp_company_name << endl;
+	cout << a3.emp_id << "\t" <<a3.emp_name <<"\t" << a3.emp_age <<"\t" << a3.emp_role <<"\t" << a3.emp_salary <<"\t" << a3.emp_city <<"\t" << a3.emp_experience <<"\t" << a3.emp_company_name << endl;
+	cout << a4.emp_id << "\t" <<a4.emp_name <<"\t" << a4.emp_age <<"\t" << a4.emp_role <<"\t" << a4.emp_salary <<"\t" << a4.emp_city <<"\t" << a4.emp_experience <<"\t" << a4.emp_company_name << endl;
+	cout << a5.emp_id << "\t" <<a5.emp_name <<"\t" << a5.emp_age <<"\t" << a5.emp_role <<"\t" << a5.emp_salary <<"\t" << a5.emp_city <<"\t" << a5.emp_experience <<"\t" << a5.emp_company_name << endl;
 }
