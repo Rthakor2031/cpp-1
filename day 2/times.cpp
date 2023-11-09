fa875621bf90d32aa245ae68506604c503d08492
Akshar Patel <143181114+Aksharpatel06@users.noreply.github.com>
2023-11-09 14:58:45 +0530

    Update times.cpp

--- day 2/times.cpp
+++ day 2/times.cpp
@@ -8,13 +8,6 @@ main()
 	int hour = sec/3600;
 	int min=sec/60;
 	sec=sec%60;
-	if(sec==60)
-	{
-		sec=0;
-	}
-	if(min>=60)
-	{
-		min=min%60;
-	}
+	min=min%60;
 	cout << hour<<":"<<min<<":"<<sec;
 }
